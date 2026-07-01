3f42d5f58bcdb8b2ff603c2e95917ae79a23068b
KIM <respm9011@gmail.com>
2026-07-01 18:29:56 +0900

    Revert "123"
    This reverts commit 126bec094465f9de33ce4e067d16145ee31f2a74.

--- Assets/01.Script/Inventory/Item.cs
+++ Assets/01.Script/Inventory/Item.cs
@@ -15,9 +15,6 @@ public class Item
     public string itemName;
     public Sprite itemImg;
     public string itemDesc;
-    public int currentCount = 1;
-    public bool isStackable;
-
     public int amount;
 
     public bool Use(PlayerCharacter player)
